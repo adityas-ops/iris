7715ade3ddaf7273a56429ebdbc3622a0802f310
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-08-12 00:10:16 +0530

    a

---
+++ components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+        <div style={{
+            paddingTop:"calc(38vmax / 10)",
+            paddingBottom:"calc(38vmax / 10)}"
+        }} className=' bg-[#FF0C54]  w-full h-full'>
+    <div className=' w-full relative '>
+
+    </div>
+        </div>
+    </>
+  )
+}
+
+export default Footer
