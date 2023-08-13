5e1f2479cf06cf7cb41ba2d9e52b18530e3f4e1c
adityas-opse <85329735+adityas-ops@users.noreply.github.com>
2023-08-13 23:18:42 +0530

    complete footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,12 +12,12 @@ function Footer() {
         }}
         className=" bg-[#FF0C54]  w-full h-full"
       >
-        <div className=" w-full relative ">
+        <div className=" w-full relative py-[30px] sm:py-0 ">
          
             <div className=" w-full h-full flex items-center justify-center">
-              <div className="w-[70%] pb-[50px] flex ">
-                <div className="w-[190px]">
-                  <p className=" font-Fira font-bold tracking-[2px] ml-[10px] text-white text-[24px] ">
+              <div className="sm:w-[70%] w-full sm:pb-[50px] flex sm:flex-row  flex-col">
+                <div className="sm:w-[190px] w-full flex flex-wrap sm:block">
+                  <p className=" font-Fira w-[40%] font-bold tracking-[2px] ml-[20px] sm:ml-[10px] text-white text-[24px] ">
                     {" "}
                     Iris
                   </p>
@@ -38,10 +38,13 @@ function Footer() {
                       <img src="/images/footer/svgexport-13.png" />
                     </Link>
                   </div>
-                  <img src="https://images.squarespace-cdn.com/content/v1/642200bb5393b3746af0a1f1/f675f7a4-68ea-4ddd-9ba8-96a0ca58f5a7/Temporary+App+Icon.png?format=300w" alt="as"  className=" ml-[10px] h-[88px] mt-[12px]"/>
+                 <div className=" mt-[30px] w-full h-full flex items-center justify-center ">
+                 <img src="https://images.squarespace-cdn.com/content/v1/642200bb5393b3746af0a1f1/f675f7a4-68ea-4ddd-9ba8-96a0ca58f5a7/Temporary+App+Icon.png?format=300w" className="ml-[10px] sm:h-[88px] h-[200px] mt-[12px]" alt="as" />
+                 </div>
                 </div>
                 {/* second column  */}
-              <div className=" ml-[40px] h-full w-[190px]">
+                <div className=" flex  sm:block mt-[40px]">
+              <div className=" sm:ml-[40px] ml-4 h-full w-[190px]">
                 <Link href="/join" className=" font-Roboto font-[300] mb-[1rem] uppercase underline text-white">
                     <p  className=" mb-[1rem] mt-[7px]" style={{
                         fontSize:"calc((0.9) - 1) * 1.2vw + 1rem)"
@@ -54,19 +57,19 @@ function Footer() {
                 </div>
               </div>
               {/* third column */}
-              <div className=" w-[190px] ml-[40px]">
+              <div className=" w-[190px] ml-0 sm:ml-[40px]">
               <Link href="/about" className=" font-Roboto font-[300] mb-[1rem] uppercase underline text-white">
                     <p  className=" mb-[1rem] mt-[7px]" style={{
                         fontSize:"calc((0.9) - 1) * 1.2vw + 1rem)"
                     }}>About</p>
                 </Link>
                 <Link href="/about" className=" font-Roboto font-[300] mb-[1rem] uppercase underline text-white">
-                    <p  className=" my-[2rem]" style={{
+                    <p  className=" sm:my-[2rem] my-[1rem]" style={{
                         fontSize:"calc((0.9) - 1) * 1.2vw + 1rem)"
                     }}>terms of service</p>
                 </Link>
-                <Link href="/partner" className=" font-Roboto font-[300] mb-[1rem] uppercase underline text-white">
-                    <p  className=" mb-[1rem] mt-[7px]" style={{
+                <Link href="/partner" className=" font-Roboto font-[300]  uppercase underline text-white">
+                    <p   className=" mb-[1rem] mt-0 sm:mt-[7px]" style={{
                         fontSize:"calc((0.9) - 1) * 1.2vw + 1rem)"
                     }}>
                         iris partner program
@@ -74,7 +77,7 @@ function Footer() {
                 </Link>
               </div>
               </div>
-              
+              </div>
             </div>
         </div>
       </div>
